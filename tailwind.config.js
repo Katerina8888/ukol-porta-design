48cf63dadd0278bbf92d8acef33a06e5a92bcecf
katerina <fischer.katerina@gmail.com>
2024-11-06 14:48:45 +0100

    feat(tailwind): add custom variables to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js}"
-  ],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontSize: {
-        'xs': '12px',
-        's': '14px',
-        'm': '16px',
-        'l': '18px',
-        'xl': '20px',
-        '2xl': '26px',
+        xs: "12px",
+        s: "14px",
+        m: "16px",
+        l: "18px",
+        xl: "20px",
+        "2xl": "26px",
       },
       fontFamily: {
-        sans: ['Plus Jakarta Sans', 'Arial', 'sans-serif'],
+        sans: ["Plus Jakarta Sans", "Arial", "sans-serif"],
+      },
+      colors: {
+        primary: "#063436",
+        secondary: "#C6E51B",
+        tertiary: "#B0D9D4",
+        quaternary: "#B0D9D433",
       },
     },
   },
